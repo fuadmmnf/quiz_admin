569ac426b5c294fe81db3f7dcf40aa9d6ab13411
fuadmmnf <bsse0920@iit.du.ac.bd>
2023-06-19 12:23:42 +0600

    nuxt3 quasar init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <!--    <q-btn color="primary" label="Primary" />-->
+    <!--    <QBtn color="secondary" label="Secondary" />-->
+    <!--    <LazyQBtn color="amber" glossy label="Amber" />-->
+  </div>
+</template>
