db8e85b59fdf021ce9464c37a586f0645de1dc78
fuadmmnf <bsse0920@iit.du.ac.bd>
2023-09-01 00:17:01 +0600

    adding tunnel network again

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -10,7 +10,7 @@ const api = axios.create({
   baseURL: process.env.DEV
     ? "http://api.apiato.test/v1"
     // : "https://api-monolith.theeduaid.com/v1",
-    : "https://139.180.128.249/v1",
+    : "http://139.180.128.249/v1",
   headers: { "Content-Type": "application/json" },
 });
 
