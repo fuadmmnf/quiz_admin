2ce237dd980fa50ce175f46e4eae32a9d5a388de
fuadmmnf <bsse0920@iit.du.ac.bd>
2023-09-25 18:03:17 +0600

    cat sub create page modified

--- src/components/tree-table/SimpleHierarchy.vue
+++ src/components/tree-table/SimpleHierarchy.vue
@@ -4,15 +4,15 @@
       <template v-slot:body="props">
         <td data-th="Name">
           <div
-            v-bind:style="props.setPadding(props.item)"
-            :class="props.iconName(props.item) != 'done' ? 'q-pl-lg' : ''"
+              v-bind:style="props.setPadding(props.item)"
+              :class="props.iconName(props.item) != 'done' ? 'q-pl-lg' : ''"
           >
             <q-btn
-              @click="props.toggle(props.item)"
-              v-if="props.iconName(props.item) != 'done'"
-              :icon="props.iconName(props.item)"
-              flat
-              dense
+                @click="props.toggle(props.item)"
+                v-if="props.iconName(props.item) != 'done'"
+                :icon="props.iconName(props.item)"
+                flat
+                dense
             >
             </q-btn>
             <span class="q-ml-sm">{{ props.item.name }}</span>
@@ -30,49 +30,53 @@
           >
           </q-btn> -->
           <q-btn
-            @click="deleteItem(props.item)"
-            icon="delete"
-            size="sm"
-            flat
-            dense
-            round
-            color="negative"
+              @click="deleteItem(props.item)"
+              icon="delete"
+              size="sm"
+              flat
+              dense
+              round
+              color="negative"
           >
           </q-btn></td
-      ></template>
+        ></template>
     </q-hierarchy>
   </span>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
-const columns = [
-  {
-    name: "Name",
-    required: true,
-    label: "Name",
-    align: "left",
-    field: "label",
-    // (optional) tell QHierarchy you want this column sortable
-    sortable: true,
-    // If you want different sorting icon
-    filterable: true,
-  },
-  {
-    name: "Actions",
-    label: "Actions",
-    field: "actions",
-    align: "center",
-    filterable: false,
-  },
-];
 export default {
   props: {
     page: String,
     tableData: Array,
   },
   name: "SimpleHierarchy",
+  data() {
+    return {
+      columns: [
+        {
+          name: "Name",
+          required: true,
+          label: "Name",
+          align: "left",
+          field: "name",
+          // (optional) tell QHierarchy you want this column sortable
+          sortable: true,
+          // If you want different sorting icon
+          filterable: true,
+        },
+        {
+          name: "Actions",
+          label: "Actions",
+          field: "actions",
+          align: "center",
+          filterable: false,
+        },
+      ]
+    }
+  },
   methods: {
     editItem(row) {
       this.$emit("editItem", row);
