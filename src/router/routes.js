cb4c3bbfe7acadfe7bd7697679707ddc736223cc
fuadmmnf <bsse0920@iit.du.ac.bd>
2023-09-25 18:10:45 +0600

    removed map from route

--- src/router/routes.js
+++ src/router/routes.js
@@ -78,8 +78,6 @@ const routes = [
       },
       { path: "/Dashboard2", component: () => import("pages/Dashboard2.vue") },
       { path: "/Profile", component: () => import("pages/UserProfile.vue") },
-      { path: "/Map", component: () => import("pages/Map.vue") },
-      { path: "/MapMarker", component: () => import("pages/MapMarker.vue") },
       { path: "/TreeTable", component: () => import("pages/TreeTable.vue") },
       { path: "/StreetView", component: () => import("pages/StreetView.vue") },
       { path: "/Cards", component: () => import("pages/Cards.vue") },
