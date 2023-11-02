06765a8df1a676787cf725097cc36d3ab17b50fa
Sultana Marjan <63195819+marjan12@users.noreply.github.com>
2023-11-02 06:33:24 +0000

    roll back SendMessage page

--- src/pages/SendMessage.vue
+++ src/pages/SendMessage.vue
@@ -1,40 +1,5 @@
 <template>
   <q-page class="q-pa-sm">
-    <q-dialog v-model="showDialog" :position="'top'">
-      <q-card flat bordered class="my-card" style="width: 60%">
-        <q-card-section>
-          <div class="text-h6">Select receiver</div>
-        </q-card-section>
-
-        <q-separator inset />
-
-        <q-card-section>
-          <div class="q-gutter-sm">
-            <q-select
-              filled
-              v-model="dialogResult"
-              input-debounce="0"
-              :options="receiverOptions"
-              stack-label
-              use-input
-              @filter="dialogFilterFn"
-            >
-              <template v-slot:no-option>
-                <q-item>
-                  <q-item-section class="text-grey">
-                    No results
-                  </q-item-section>
-                </q-item>
-              </template>
-            </q-select>
-
-            <q-badge color="secondary" class="q-mb-md">
-              Selected: {{ dialogResult }}
-            </q-badge>
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-dialog>
     <q-card class="no-shadow" bordered>
       <q-card-section class="row items-center justify-between">
         <div class="text-h6 text-indigo-8">
@@ -53,7 +18,7 @@
         <div class="col-6">
           <q-card class="no-shadow" bordered>
             <q-card-section class="row items-center justify-between">
-              <div class="text-h6">{{ dialogResult }}</div>
+              <div class="text-h6">Receiver Configuration</div>
             </q-card-section>
             <q-card-section>
               <q-list>
@@ -120,43 +85,21 @@
     </div>
   </q-page>
 </template>
+
 <script>
-import { onMounted, ref, watch } from "vue";
+import { ref } from "vue";
 
 const stringOptions = ["Sultana", "Marjan", "Mitu"];
-const receiverOptions = ["abc", "def", "ghi"];
 
 export default {
   setup() {
     const options = ref(stringOptions);
-    const dialogReceiverOptions = ref(receiverOptions);
-    const showDialog = ref(false);
-    const dialogResult = ref([]); // Define dialogResult as a ref
-
-    onMounted(() => {
-      showDialog.value = true;
-    });
-
-    // Watch for changes in dialogResult
-    watch(dialogResult, (newResult) => {
-      if (newResult !== "") {
-        showDialog.value = false;
-      }
-      console.log("dialogResult:", newResult);
-    });
-
-    const receiverOption = ref("all");
-    const modelMultiple = ref([]);
 
     return {
-      receiverOption,
-      modelMultiple,
-      dialogResult,
-      showDialog,
+      receiverOption: ref("all"),
+      modelMultiple: ref([]),
       stringOptions,
       options,
-      dialogReceiverOptions,
-      receiverOptions,
 
       filterFn(val, update) {
         if (val === "") {
@@ -169,22 +112,6 @@ export default {
         update(() => {
           const needle = val.toLowerCase();
           options.value = stringOptions.filter(
-            (v) => v.toLowerCase().indexOf(needle) > -1
-          );
-        });
-      },
-
-      dialogFilterFn(val, update) {
-        if (val === "") {
-          update(() => {
-            dialogReceiverOptions.value = receiverOptions;
-          });
-          return;
-        }
-
-        update(() => {
-          const needle = val.toLowerCase();
-          dialogReceiverOptions.value = receiverOptions.filter(
             (v) => v.toLowerCase().indexOf(needle) > -1
           );
         });
@@ -193,70 +120,3 @@ export default {
   },
 };
 </script>
-
-<!-- <script>
-import { onMounted, ref, watch } from "vue";
-
-const stringOptions = ["Sultana", "Marjan", "Mitu"];
-const receiverOptions = ["abc", "def", "ghi"];
-
-export default {
-  setup() {
-    const options = ref(stringOptions);
-    const dialogReceiverOptions = ref(receiverOptions);
-    const showDialog = ref(false);
-
-    onMounted(() => {
-      showDialog.value = true;
-    });
-    watch(dialogResult, (newResult) => {
-      if (newResult !== "") {
-        closeDialog();
-      }
-    });
-    return {
-      receiverOption: ref("all"),
-      modelMultiple: ref([]),
-      dialogResult: ref([]),
-      showDialog,
-      stringOptions,
-      options,
-
-      dialogReceiverOptions,
-      receiverOptions,
-
-      filterFn(val, update) {
-        if (val === "") {
-          update(() => {
-            options.value = stringOptions;
-          });
-          return;
-        }
-
-        update(() => {
-          const needle = val.toLowerCase();
-          options.value = stringOptions.filter(
-            (v) => v.toLowerCase().indexOf(needle) > -1
-          );
-        });
-      },
-
-      dialogFilterFn(val, update) {
-        if (val === "") {
-          update(() => {
-            dialogReceiverOptions.value = receiverOptions;
-          });
-          return;
-        }
-
-        update(() => {
-          const needle = val.toLowerCase();
-          dialogReceiverOptions.value = receiverOptions.filter(
-            (v) => v.toLowerCase().indexOf(needle) > -1
-          );
-        });
-      },
-    };
-  },
-};
-</script> -->
