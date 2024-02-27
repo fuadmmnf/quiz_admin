c2cdefb94c1211ba11b450f9872cdd42bcdc26b5
fuadmmnf <bsse0920@iit.du.ac.bd>
2024-02-28 00:19:04 +0600

    fixed blog index

--- src/pages/Blog/Index.vue
+++ src/pages/Blog/Index.vue
@@ -3,29 +3,17 @@
     <q-card class="no-shadow" bordered>
       <q-card-section class="row items-center justify-between">
         <div class="text-h6 text-indigo-8">
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-          {{"Course : "+decodeURIComponent(route.query.course_name)}} Class Materials
-          <div class="text-subtitle2">
-            List of {{"Course : "+decodeURIComponent(route.query.course_name)}} resources are shown here
-========
           Blogs
           <div class="text-subtitle2">
             List of {{ route.params.status }} blogs are shown here
->>>>>>>> dev:src/pages/Blog/Index.vue
           </div>
         </div>
         <div class="row">
           <q-btn
             color="primary"
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-            label="Add Material"
-            icon="add"
-            :to="{name: 'classmaterial-create', query: route.query}"
-========
             label="Add Blog"
             icon="add"
             :to="{name: 'blog-create', query: {course_id:  this.$route.query.course_id?.length? this.$route.query.course_id: ''}}"
->>>>>>>> dev:src/pages/Blog/Index.vue
           />
         </div>
       </q-card-section>
@@ -38,11 +26,7 @@
             <q-card-section>
               <q-table
                 :columns="columns"
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-                :rows="classMaterials"
-========
                 :rows="blogs"
->>>>>>>> dev:src/pages/Blog/Index.vue
                 :loading="loading"
                 :rows-per-page-options="[10]"
                 row-key="real_id"
@@ -61,25 +45,6 @@
                       {{ props.row.title }}
                     </q-td>
 
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-                    <q-td key="type" :props="props">
-                      {{ props.row.type }}
-                    </q-td>
-                    <q-td key="category" :props="props">
-                      {{ props.row.category?.data.name ?? "" }}
-                    </q-td>
-                    <q-td key="subject" :props="props">
-                      {{ props.row.subject?.data.name ?? "" }}
-                    </q-td>
-                    <q-td key="faculty" :props="props">
-                      {{ props.row.faculty?.data.name ?? "" }}
-                    </q-td>
-                    <q-td key="course" :props="props">
-                      {{ props.row.course?.data.title ?? "" }}
-                    </q-td>
-                    <q-td key="time" :props="props">
-                      {{ props.row.time }}
-========
                     <q-td key="category" :props="props">
                       {{ props.row.category?.data.name }}
                     </q-td>
@@ -91,7 +56,6 @@
                     </q-td>
                     <q-td key="read_count" :props="props">
                       {{ props.row.read_count }}
->>>>>>>> dev:src/pages/Blog/Index.vue
                     </q-td>
 
                     <q-td key="action" :props="props">
@@ -102,11 +66,7 @@
                         round
                         dense
                         flat
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-                        :to="{name: 'classmaterial-edit', params: {id: props.row.id}}"
-========
                         :to="{name: 'blog-edit', params: {id: props.row.id}}"
->>>>>>>> dev:src/pages/Blog/Index.vue
                       />
                       <q-btn
                         v-if="props.row.status === 'draft'
@@ -161,26 +121,15 @@ import {api} from "boot/axios";
 import {useQuasar} from "quasar";
 import {getLectureClasses} from "src/services/course_service";
 import {useRoute} from "vue-router";
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-import {updateClassMaterialStatus} from "src/services/classmaterial_service";
-
-export default defineComponent({
-  name: "ClassMaterials",
-========
 import {updateBlog} from "src/services/blog_service";
 
 export default defineComponent({
   name: "Blogs",
->>>>>>>> dev:src/pages/Blog/Index.vue
   setup() {
     const {$q} = useQuasar();
     const store = useStore();
     const route = useRoute();
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-    const classMaterials = ref([]);
-========
     const blogs = ref([]);
->>>>>>>> dev:src/pages/Blog/Index.vue
     const pagination = ref({
       page: 1,
       rowsPerPage: 10,
@@ -189,26 +138,15 @@ export default defineComponent({
     const searchData = ref({type: "", keywords: ""});
 
     const loading = ref(true);
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-    const fetchClassMaterials = async (page = 1) => {
-========
     const fetchBlogs = async (page = 1) => {
->>>>>>>> dev:src/pages/Blog/Index.vue
       loading.value = true;
 
       api
         .get(
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-          `/class-materials?search=status:${route.params.status}${route.query.course_id?.length ? `;course_id:${route.query.course_id}` : ''}&include=course,category,faculty,subject&orderBy=id&sortedBy=desc&searchJoin=and&page=${page}`
-        )
-        .then((response) => {
-          classMaterials.value = response.data.data;
-========
           `/blogs?search=status:${route.params.status}&include=category&orderBy=id&sortedBy=desc&searchJoin=and&page=${page}`
         )
         .then((response) => {
           blogs.value = response.data.data;
->>>>>>>> dev:src/pages/Blog/Index.vue
           const meta = response.data.meta.pagination;
           pagination.value = {
             page: meta.current_page,
@@ -224,24 +162,15 @@ export default defineComponent({
         });
     };
     const onRequest = (props) => {
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-      fetchClassMaterials(props.pagination.page);
-========
       fetchBlogs(props.pagination.page);
->>>>>>>> dev:src/pages/Blog/Index.vue
     };
     return {
       store,
       route,
       pagination,
       loading,
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-      fetchClassMaterials,
-      classMaterials,
-========
       fetchBlogs,
       blogs,
->>>>>>>> dev:src/pages/Blog/Index.vue
       $q,
       searchData,
       onRequest,
@@ -258,21 +187,6 @@ export default defineComponent({
           label: "Title",
           field: "title",
           align: "left",
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-        },
-        {
-          name: "type",
-          label: "TYpe",
-          field: "type",
-          align: "left",
-        },
-        {
-          name: "category",
-          label: "Category",
-          field: "category",
-          align: "left",
-========
->>>>>>>> dev:src/pages/Blog/Index.vue
         },
         {
           name: "category",
@@ -281,23 +195,6 @@ export default defineComponent({
           align: "left",
         },
         {
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-          name: "faculty",
-          label: "Faculty",
-          field: "faculty",
-          align: "left",
-        },
-        {
-          name: "course",
-          label: "Course",
-          field: "course",
-          align: "left",
-        },
-        {
-          name: "time",
-          label: "Time",
-          field: "time",
-========
           name: "author_info",
           label: "Author Info",
           field: "course",
@@ -313,7 +210,6 @@ export default defineComponent({
           name: "read_count",
           label: "Read Count",
           field: "read_count",
->>>>>>>> dev:src/pages/Blog/Index.vue
           align: "left",
         },
         {
@@ -330,52 +226,27 @@ export default defineComponent({
   methods: {
     async handleStatusChangeClick(selectedItem, stat) {
 
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-      const {status, error} = await updateClassMaterialStatus(selectedItem.id, {
-========
       const {status, error} = await updateBlog(selectedItem.id, {
->>>>>>>> dev:src/pages/Blog/Index.vue
         status: stat,
       });
 
       if (status === 200) {
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-        console.log("Class Material status updated successfully.");
-        await this.fetchClassMaterials();
-      } else {
-        console.error("Error updating class material:", error);
-========
         console.log("Blog status updated successfully.");
         await this.fetchBlogs();
       } else {
         console.error("Error updating blog:", error);
->>>>>>>> dev:src/pages/Blog/Index.vue
       }
     },
     onDelete(id) {
       this.$q
         .dialog({
           title: "Confirm",
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-          message: "Would you like to delete the class material?",
-========
           message: "Would you like to delete the blog?",
->>>>>>>> dev:src/pages/Blog/Index.vue
           cancel: true,
           persistent: true,
         })
         .onOk(() => {
           api
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-            .delete(`/class-materials/${id}`)
-            .then((res) => {
-              this.$q.notify({
-                message: "Class material deleted successfully",
-                color: "positive",
-                icon: "check",
-              });
-              this.fetchClassMaterials();
-========
             .delete(`/blogs/${id}`)
             .then((res) => {
               this.$q.notify({
@@ -384,7 +255,6 @@ export default defineComponent({
                 icon: "check",
               });
               this.fetchBlogs();
->>>>>>>> dev:src/pages/Blog/Index.vue
             })
             .catch((err) => {
               console.log(err);
@@ -397,11 +267,7 @@ export default defineComponent({
   },
 
   mounted() {
-<<<<<<<< HEAD:src/pages/ClassMaterial/index.vue
-    this.fetchClassMaterials();
-========
     this.fetchBlogs();
->>>>>>>> dev:src/pages/Blog/Index.vue
   },
 });
 </script>
